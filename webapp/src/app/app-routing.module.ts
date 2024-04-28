6b15bfc11dd98613bcf1b8231377510fad66c6fd
Ryan OQLIS <108818342+ryanshaxx@users.noreply.github.com>
2024-04-28 17:58:25 +0200

    modified git is wack

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './feature/screens/landing/landing.component';
+import { AboutComponent } from './feature/screens/about/about.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', component: LandingComponent },
+  { path: 'about', component: AboutComponent },
   // more routes here
 ];
 
--- webapp/src/app/feature/screens/screens.module.ts
+++ webapp/src/app/feature/screens/screens.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    AboutComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    RouterModule
   ]
 })
 export class ScreensModule { }
