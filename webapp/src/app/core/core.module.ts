1ec961351ad46a57c6a6af8b9333f96d7adca77d
Connor McDonald <connormcd98@gmail.com>
2024-04-28 09:35:24 +0200

    add exports to core module

--- webapp/src/app/core/core.module.ts
+++ webapp/src/app/core/core.module.ts
@@ -13,6 +13,10 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
